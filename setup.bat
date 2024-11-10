f31f3d0a925ac568af8f25b55885eb30b2dd10a0
Manuel Bildhauer <manub211@gmail.com>
2024-11-10 19:43:59 +0100

    setup maven config

---
+++ setup.bat
@@ -0,0 +1,4 @@
+mvn install:install-file -Dfile=lib/jbpt-0.2.348.jar -DgroupId=org.jbpt -DartifactId=jbpt -Dversion=0.2.348 -Dpackaging=jar
+mvn install:install-file -Dfile=lib/jxl.jar -DgroupId=jxl -DartifactId=jxl -Dversion=1 -Dpackaging=jar
+mvn install:install-file -Dfile=lib/opencsv-2.3.jar -DgroupId=au.com.opencsv -DartifactId=opencsv -Dversion=2.3 -Dpackaging=jar
+mvn install:install-file -Dfile=lib/collections-generic-4.01.jar -DgroupId=org.apache.commons.collections15 -DartifactId=collections-generic -Dversion=4.01 -Dpackaging=jar
